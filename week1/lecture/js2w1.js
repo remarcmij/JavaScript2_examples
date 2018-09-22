a5b45577b86479b62fe23c201086feab8c9b27ac
daniela-olaru <43111296+daniela-olaru@users.noreply.github.com>
2018-09-22 12:59:12 +0200

    Update js2w1.js

--- week1/lecture/js2w1.js
+++ week1/lecture/js2w1.js
@@ -1,57 +1,6 @@
 {
-  const maartjesWork = [
-    {
-      task: 'Write a summary HTML/CSS',
-      done: false
-    },
-    {
-      task: 'Some web development',
-      done: true
-    },
-    {
-      task: 'Fix homework for class10',
-      done: false
-    },
-    {
-      task: 'Talk to a lot of people',
-      done: false
-    },
-    {
-      task: 'Keep writing summary',
-      done: false
-    },
-    {
-      task: 'Some more web development',
-      done: false
-    },
-    {
-      task: 'Staring out the window',
-      done: false
-    },
-    {
-      task: 'Talk to a lot of people',
-      done: false
-    },
-    {
-      task: 'Look at application assignments new students',
-      done: false
-    }
-  ];
 
-  function render(todos, container) {
-    for (let i = 0; i < todos.length; i++) {
-      const todo = todos[i];
-      const li = document.createElement('li');
-      container.appendChild(li);
-      li.innerText = todo.task;
-      if (todo.done) {
-        li.setAttribute('style', 'text-decoration: line-through');
-      }
-    }
-  }
-
-
-  function main() {
+function main() {
 
     // DOM MANIPULATIONS
 
@@ -79,19 +28,30 @@
     colorElem.style.color = "magenta";
     colorElem.innerText = "I am magenta text.";
 
-    // 4. Create Elements
-    const rootElem = document.getElementById('root');
+    // 4. Create Elements - DEMO
+    /*const rootElem = document.getElementById('root');
     const h1Elem = document.createElement('h1');
     h1Elem.innerText = 'Maartjes work';
     rootElem.appendChild(h1Elem);
     const ulElem = document.createElement('ul');
     rootElem.appendChild(ulElem);
-    render(maartjesWork, ulElem);
+    render(maartjesWork, ulElem); */
 
-    // EVENTS
+    // EVENTS & EVENTS PROPAGATION
 
     // 1. addEventListener
-    // a. on the main window
+    // a. on a specific dom element:
+    const parentElem = document.getElementById("parent-element");
+    parentElem.addEventListener("click", function () {
+      console.log("Handler for parent element.");
+    });
+    
+    pizzaSelectElem = document.getElementById("pizza-price");
+    pizzaSelectElem.addEventListener("change", function(event) {
+    	console.log("You selected a " + pizzaSelectElem.options[pizzaSelectElem.selectedIndex].text + " pizza and the option's value is " + pizzaSelectElem.value);
+    })
+
+    // b. on the main window
     window.addEventListener("click", function () {
       // console.log("You knocked?");
     });
@@ -99,55 +59,8 @@
     // using arrow function - just worth mentioning, not going too much in detail
     // window.addEventListener("click", () => {
     // 	console.log("You knocked?");
-    // });
-
-    // b. on a specific dom element:
-    const buttonElem = document.getElementById("button");
-    buttonElem.addEventListener("click", function () {
-      console.log("Button clicked, button's listener executed!");
-    });
-
-    // 2. removeEventListener
-    const buttonOnceElem = document.getElementById("button-once");
-    function buttonOnceClicked() {
-      console.log("Button clicked, button's handler executed only the first time!");
-      buttonOnceElem.removeEventListener("click", buttonOnceClicked);
-    }
-    buttonOnceElem.addEventListener("click", buttonOnceClicked);
-
-    // EVENT OBJECTS
-    const buttonForMouseElem = document.getElementById("button-for-mouse");
-    buttonForMouseElem.addEventListener("mousedown", function (event) {
-      console.log("This was an event of type:" + event.type);
-      if (event.button === 0) {
-        console.log("Left button");
-      } else if (event.button === 2) {
-        console.log("Right button");
-      }
-    });
-
-    // PROPAGATION
-    const parentElem = document.getElementById("parent-element");
-    const childElem = document.getElementById("child-element");
-    parentElem.addEventListener("mousedown", function () {
-      console.log("Handler for parent element.");
-    });
-    // if right button of the mouse => parent element handler not executed, otherwise it is
-    childElem.addEventListener("mousedown", function (event) {
-      if (event.button === 2) {
-        console.log("You clicked with the right button, parent handler not executed!");
-        event.stopPropagation();
-      } else {
-        console.log("You didn't click  with the right button, parent handler executed!");
-      }
-    });
-
+    // });    
   }
 
   window.onload = main;
-
-  //TODO: adding multiple listeners for the same event, to the same element
-  //TODO: illustrate capturing, bubbling
-  //TODO: event delegation ?!
-  //TODO: event.target <-> event.currentTarget ?!
 }
