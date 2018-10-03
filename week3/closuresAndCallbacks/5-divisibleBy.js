7e1daa73540791384da048c98e71a8eca97314f6
Jim Cramer <remarcmij@gmail.com>
2018-10-03 16:52:28 +0200

    Removed block scope (not necessary with Node)

--- week3/closuresAndCallbacks/5-divisibleBy.js
+++ week3/closuresAndCallbacks/5-divisibleBy.js
@@ -1,11 +1,11 @@
 'use strict';
-{
-  const divisibleBy = numerator => value => value % numerator === 0;
 
-  const divisibleBy3 = divisibleBy(3);
+const divisibleBy = numerator => value => value % numerator === 0;
 
-  console.log(divisibleBy3(8));
-  console.log(divisibleBy3(9));
+const divisibleBy3 = divisibleBy(3);
 
-  console.log(divisibleBy(11)(66));
-}
+console.log(divisibleBy3(8));
+console.log(divisibleBy3(9));
+
+console.log(divisibleBy(11)(66));
+
