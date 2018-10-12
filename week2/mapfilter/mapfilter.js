07cf09ad5030f87cc68a76c2cdcd838be5ee4b39
Jim Cramer <remarcmij@gmail.com>
2018-10-12 14:31:58 +0200

    Updated map & filter examples

--- week2/mapfilter/mapfilter.js
+++ week2/mapfilter/mapfilter.js
@@ -5,39 +5,54 @@
 
 const numbers = [1, 2, 3, 4];
 
-function myMap(arr, mapFn) {
+function map(arr, mapFn) {
   const result = [];
   for (let i = 0; i < arr.length; i++) {
-    const mappedValue = mapFn(arr[i]);
+    const elem = arr[i];
+    const mappedValue = mapFn(elem, i, arr);
     result.push(mappedValue);
   }
   return result;
 }
 
-console.log(myMap(numbers, x => x * x));
+console.log(map(numbers, x => x * x));
 console.log(numbers.map(x => x * x));
 
-function myFilter(arr, predicateFn) {
+function filter(arr, predicateFn) {
   const result = [];
   for (let i = 0; i < arr.length; i++) {
-    if (predicateFn(arr[i])) {
+    const elem = arr[i];
+    if (predicateFn(elem, i, arr)) {
       result.push(arr[i]);
     }
   }
   return result;
 }
 
-console.log(myFilter(numbers, x => x % 2 === 0));
+console.log(filter(numbers, x => x % 2 === 0));
 console.log(numbers.filter(x => x % 2 === 0));
 
-function myForEach(arr, func) {
+function reduce(arr, reducerFn, initialValue) {
+  let accumulator = initialValue;
   for (let i = 0; i < arr.length; i++) {
-    func(arr[i]);
+    const elem = arr[i];
+    accumulator = reducerFn(accumulator, elem, i, arr);
+  }
+  return accumulator;
+}
+
+console.log(reduce(numbers, (acc, number) => acc + number, 0));
+console.log(numbers.reduce((acc, number) => acc + number, 0));
+
+function forEach(arr, func) {
+  for (let i = 0; i < arr.length; i++) {
+    const elem = arr[i];
+    func(elem, i, arr);
   }
 }
 
 let sum = 0;
-myForEach(numbers, x => sum += x);
+forEach(numbers, x => sum += x);
 console.log(sum);
 
 sum = 0;
